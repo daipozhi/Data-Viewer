5764bfeec6588cddf8f1a1b19b62a914cde05f18
daipozhi <daipozhi@sina.com>
2021-09-10 10:47:31 +0000

    12th_commit

---
+++ bowo-5.1-src-linux-b1/make.bat
@@ -0,0 +1,2 @@
+rm gcc_bowo.exe
+g++ -o gcc_bowo.exe vc50_bmp.cpp vc50_buff.cpp vc50_edit.cpp vc50_font.cpp vc50_fldstru.cpp vc50_getread.cpp vc50_inkey.cpp vc50_link.cpp vc50_make1.cpp vc50_main.cpp vc50_mbase.cpp vc50_menuvar.cpp vc50_print.cpp vc50_user.cpp vc50_win.cpp vc50_xbase.cpp gcc_win32simulator.cpp
